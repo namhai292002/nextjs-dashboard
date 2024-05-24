e13f29c41ae6dd0817cca335be0199f119b7f79d
Nguyen Nam Hai <hainn@vnb.local>
2024-05-24 10:17:25 +0700

    update fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import { Charis_SIL, Roboto_Mono } from 'next/font/google';
+import { Charis_SIL, Roboto_Mono, Lusitana } from 'next/font/google';
 
 export const inter = Charis_SIL({
   subsets: ['latin'],
@@ -8,4 +8,9 @@ export const inter = Charis_SIL({
 export const roboto_mono = Roboto_Mono({
   subsets: ['latin'],
   display: 'swap',
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
 });
